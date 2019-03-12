8daad9cf696e9493b7ad45618fe33068888205e4
Kyle Storey <kstorey2@ssh3.et.byu.edu>
2019-03-12 16:04:32 -0600

    Add control.h

---
+++ control/control.h
@@ -0,0 +1,17 @@
+#include <stdio.h> //we will want some debug output
+
+typedef struct dPos { //a position
+    double x //the drones latitude (relative to position on boot)
+    double y //the drones longitude
+    double z //the drones altitude
+} 
+
+typedef struct dOri { //a position
+    double rol //the roll  
+    double pit //the pitch
+    double yaw //the yaw
+} 
+
+void setMove(double x, double y, double z);
+
+void setTurn(double yaw);
